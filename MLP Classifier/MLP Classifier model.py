a64763842b62faae205705d7569ff14b24f1d007
jasimusmani <mohdjasimusmani@gmail.com>
2022-04-06 14:44:11 -0400

    MLP Classifier model

--- MLP Classifier/MLP Classifier model.py
+++
@@ -1,9 +0,0 @@
-#MLP
-
-from sklearn.neural_network import MLPClassifier
-MLPClassifier = sklearn.neural_network.MLPClassifier(hidden_layer_sizes=(100,),
-                                     activation='relu', solver='adam',batch_size='auto',
-                                     learning_rate='constant', learning_rate_init=0.001)
-MLP = MLPClassifier.fit(X_trainsvm, ytrainsvm)
-print(MLP.score(X_testsvm, y_test_))
-
